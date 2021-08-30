baa1201841a0ca79dd135847ef0832e5bea1497b
Demchenko Kirill <thegalaxydias@gmail.com>
2021-08-30 20:38:33 +0300

    Добавьте файлы проекта.

---
+++ Construct/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Constructor
+{
+
+}
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        User tom = new User("admin1", 12345);
+
+        tom.description();
+
+
+        Console.ReadKey();
+    }
+
+
+}
+
+
+public class User
+{
+    public string role;
+    public string login;
+    public int password;
+
+
+
+    public User(string l, int p) { role = "Unknown"; login = l; password = p; }
+
+    public void description()
+    {
+        Console.WriteLine($"Role: { role} \nLogin: { login} \nPassword: {password}");
+    }
+
+
+}
+
